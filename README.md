4db9f2ab1089f8fa112f6c2588356f25cfdea711
pedrodornela <pedro.henriquedgoncalves@gmail.com>
2022-11-11 22:32:23 -0300

    Criando modulos e pacotes em Python

--- README.md
+++ README.md
@@ -2,26 +2,27 @@
 
 Description. 
 The package package_name is used to:
-	- 
-	-
+	Processing:
+		- Histogram matching
+		- Structural similarity
+		- Resize image
+	Utils:
+		- Read image
+	 	- Save image
+		- Plot image
+		- Plot result
+		- Plot histogram
 
 ## Installation
 
 Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
 
 ```bash
-pip install package_name
-```
-
-## Usage
-
-```python
-from package_name.module1_name import file1_name
-file1_name.my_function()
+pip install PACKAGE-TEMPLATE
 ```
 
 ## Author
-My_name
+Pedro Henrique Dornela Gonçalves
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="PedroDornela",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
+    author="Pedro",
+    author_email="pedro.henriquedgoncalves@gmail.com",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/pedrodornela/package-template",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.7'
 )
